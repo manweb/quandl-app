e73a3b06f997078aee8b8110d2d61ae05d81cebc
Manuel Weber <weber_manuel@hotmail.com>
2018-06-11 17:28:44 -0700

    added plotting

--- quandl_app.py
+++ quandl_app.py
@@ -1,11 +1,31 @@
 import os
+import numpy as np
 from flask import Flask, render_template, request, redirect
+from bokeh.plotting import figure
+from bokeh.embed import components
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
 	return render_template('index.html')
+
+@app.route('/plot_data')
+def plot_data():
+	plot = get_plot()
+
+	script, div = components(plot)
+
+	return render_template('plot_data.html', script=script, div=div)
+
+def get_plot():
+	x = np.arange(1000)
+	y = np.random.random(1000)
+
+	p = figure()
+	p.line(x, y, line_color='blue')
+
+	return p
 
 if __name__ == '__main__':
 	port = int(os.environ.get('PORT', 5000))
